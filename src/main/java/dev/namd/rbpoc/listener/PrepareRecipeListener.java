6910876b92ca952792f018f75bd7231c7387dd82
NotAMojangDev <86620443+NotAMojangDev@users.noreply.github.com>
2025-08-04 01:36:54 +0300

    Replace ItemResultEvent with ClickEvent

--- src/main/java/dev/namd/rbpoc/listener/PrepareRecipeListener.java
+++ src/main/java/dev/namd/rbpoc/listener/PrepareRecipeListener.java
@@ -1,22 +1,18 @@
 package dev.namd.rbpoc.listener;
 
+import com.destroystokyo.paper.event.player.PlayerRecipeBookClickEvent;
 import dev.namd.rbpoc.util.PopulateRecipes;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.PrepareItemCraftEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.inventory.ShapedRecipe;
-
 
 public class PrepareRecipeListener implements Listener {
 
     @EventHandler
-    public void onPrepareResult(PrepareItemCraftEvent event) {
-        Player player = (Player) event.getView().getPlayer();
-        if (!(event.getRecipe() instanceof ShapedRecipe recipe)) return;
-        player.sendMessage(recipe.getKey().getNamespace() + ":" + recipe.getKey().getKey());
-
+    public void onPrepareResult(PlayerRecipeBookClickEvent event) {
+        Player player = event.getPlayer();
+        player.sendMessage(event.getRecipe().getNamespace() + ":" + event.getRecipe().getKey());
     }
 
     @EventHandler
